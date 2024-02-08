2347a5105fc4dc61e0f8fd9276c14d9f71a950b0
dangvannho <dangvannho7@gmail.com>
2024-02-08 18:30:26 +0700

    upload 8/2/2024

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+// class component
+// function compnent
+import React from "react";
+class MyComponent extends React.Component {
+  render() {
+    return <div>My first component {Math.random()}</div>;
+  }
+}
+
+export default MyComponent;
